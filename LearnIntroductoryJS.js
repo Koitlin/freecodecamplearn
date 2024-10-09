9040ffe2ca5ab18a678f24b13eaeaded2d82072e
Eugene Kotygin <kotigin@yandex.ru>
2024-10-09 21:34:19 +0300

    Step38

--- LearnIntroductoryJS.js
+++ LearnIntroductoryJS.js
@@ -1,5 +1,7 @@
-let pushed = rows.push("freeCodeCamp");
-console.log(pushed);
-let popped = rows.pop();
-console.log(popped);
-console.log(rows);
+const character = "#";
+const count = 8;
+const rows = [];
+
+for (let i = 0; i < count; i = i + 1) {
+    console.log(i);
+}
