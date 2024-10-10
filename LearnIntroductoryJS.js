6a8c182f6e20532ca8969fb512a9b36c4d49903d
Koitlin <52778931+Koitlin@users.noreply.github.com>
2024-10-10 22:54:30 +0300

    Step80

--- LearnIntroductoryJS.js
+++ LearnIntroductoryJS.js
@@ -3,17 +3,22 @@ const count = 8;
 const rows = [];
 
 function padRow(rowNumber, rowCount) {
-    return " ".repeat(rowCount - rowNumber) + character.repeat(2 * rowNumber - 1) + " ".repeat(rowCount - rowNumber);
+  return " ".repeat(rowCount - rowNumber) + character.repeat(2 * rowNumber - 1) + " ".repeat(rowCount - rowNumber);
 }
 
-for (let i = 0; i < count; i = i + 1) {
-    rows.push(padRow(i + 1, count));
+// TODO: use a different type of loop
+/*for (let i = 1; i <= count; i++) {
+  rows.push(padRow(i, count));
+}*/
+
+if (false) {
+  console.log("Condition is true");
 }
 
 let result = ""
 
 for (const row of rows) {
-    result = result + "\n" + row;
+  result = result + "\n" + row;
 }
 
 console.log(result);
