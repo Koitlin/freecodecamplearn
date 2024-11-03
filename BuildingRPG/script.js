f467f8dcc7081960c32839c910b4f03c7f575ab7
Eugene Kotygin <ekotygin@gmail.com>
2024-11-03 19:59:26 +0300

    RPG_Step100

--- BuildingRPG/script.js
+++ BuildingRPG/script.js
@@ -86,7 +86,7 @@ function buyHealth() {
 }
 
 function buyWeapon() {
-  if (currentWeaponIndex < 3) {
+  if (currentWeaponIndex < weapons.length - 1) {
     if (gold >= 30) {
       gold -= 30;
       currentWeaponIndex++;
@@ -98,8 +98,11 @@ function buyWeapon() {
     } else {
       text.innerText = "You do not have enough gold to buy a weapon.";
     }
+  } else {
+    text.innerText = "You already have the most powerful weapon!";
   }
 }
+
 
 function fightSlime() {
 
